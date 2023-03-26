2f1e06c52613009f0522d4fdc45ac1757160b7ee
lMelkorI <108741628+lMelkorI@users.noreply.github.com>
2023-03-26 22:01:46 +0300

    Add files via upload

---
+++ ödev-1.py
@@ -0,0 +1,25 @@
+
+#kodlama.io_ödev-1
+
+#Deniz DURKUT
+
+#veri tipleri
+
+#integer: Sayısal değerlere sahip veri tipi 
+#string: Metinsel ifadelere sahip veri tipi
+#float: integer gibi sayısal değere sahiptir ancak ondalıklı olarak ifade edilir
+#boolean: True or False değerine sahip veri tipi
+
+#kodlama.io daki veri tipi örnekleri
+
+#string
+odevtanimi=("Python'da Veri Tiplerini araştırınız, her bir veri tipi için kendi cümlelerinizle açıklamalar yazınız.Kodlama.io sitesinde değişken olarak kullanıldığını düşündüğünüz verileri, veri tipleriyle birlikte örneklendiriniz.Kodlama.io sitesinde şart blokları kullanıldığını düşündüğünüz kısımları örneklendiriniz ve Python dilinde bu örnekleri koda dökünüz.Ödev linkinizi buraya yorum olarak ekleyiniz.")
+
+#numeric
+#tamamlananyüzde=(Tamamlananders/toplamdersödevvedegerlendirmesayisi)*100
+
+#booleam
+if dersibitir==1:
+    Tamamlananders=Tamamlananders+1
+else:
+
